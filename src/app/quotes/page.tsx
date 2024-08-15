371b1f4af443a9647435d8c3e313d755b8bf0443
Ethan Quarry <dev.ethanquarry@gmail.com>
2024-08-16 00:23:17 +0100

    feat: added quote

--- src/app/quotes/page.tsx
+++ src/app/quotes/page.tsx
@@ -6,7 +6,8 @@ const quotes = [
     { text: "Do not despair. I know you will not despair. You have a manly and a proud heart. A proud heart can survive failure because such a failure does not prick its pride. It is more difficult and more bitter when a man fails alone.", author: "Chinua Achebe" },
     { text: "True genius, in strategy or anywhere, lies in self-control, self-mastery, presence of mind and fluidity of thought.", author: "Robert Greene" },
     { text: "There is absolutely nothing you're supposed to be doing.", author: "Eckhart Tolle" },
-    { text: "The only way to do great work is to love what you do. - Steve Jobs", author: "Steve Jobs" },
+    { text: "The only way to do great work is to love what you do.", author: "Steve Jobs" },
+    { text: "The unexamined life is not worth living.", author: "Socrates" },
 ]
 
 export default function QuotesPage() {
