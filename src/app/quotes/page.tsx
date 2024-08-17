236783ac47d5d64c867519ec61cac29eb975b1a2
Ethan Quarry <dev.ethanquarry@gmail.com>
2024-08-17 14:32:18 +0100

    feat: added quote

--- src/app/quotes/page.tsx
+++ src/app/quotes/page.tsx
@@ -4,6 +4,7 @@ import CustomLink from "@/components/ui/link"
 
 const quotes = [
     { text: "Do not despair. I know you will not despair. You have a manly and a proud heart. A proud heart can survive failure because such a failure does not prick its pride. It is more difficult and more bitter when a man fails alone.", author: "Chinua Achebe" },
+    { text: "You must assume responsibility for what you believe.", author: "Richard W. Hamming" },
     { text: "True genius, in strategy or anywhere, lies in self-control, self-mastery, presence of mind and fluidity of thought.", author: "Robert Greene" },
     { text: "There is absolutely nothing you're supposed to be doing.", author: "Eckhart Tolle" },
     { text: "The only way to do great work is to love what you do.", author: "Steve Jobs" },
