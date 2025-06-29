4859607361336c3a3a1c7b76d12f02669f1ec03e
Ethan Quarry <dev.ethanquarry@gmail.com>
2025-06-29 03:53:56 +0000

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import { Inter } from "next/font/google";
 import "./globals.css";
 import MaxWidthWrapper from "@/components/shared/max-width-wrapper";
@@ -22,6 +23,7 @@ export default function RootLayout({
             <Navbar />
           </header>
           {children}
+          <Analytics />
         </MaxWidthWrapper>
       </body>
     </html>
